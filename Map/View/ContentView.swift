5a253704b1bc6b8f01f0eccc0b83b2813b1262e7
Zohra18 <ach.zohra@gmail.com>
2020-03-18 21:44:53 +0100

    organisation added, added button for center on location, will add function after

---
+++ Map/View/ContentView.swift
@@ -0,0 +1,88 @@
+//
+//  ContentView.swift
+//  Map
+//
+//  Created by Zohra Achour on 04/03/2020.
+//  Copyright © 2020 Zohra Achour. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct ContentView: View {
+    
+    //    @State private var centerCoordinate = CLLocationCoordinate2D()
+    //    @State private var locations = [MKPointAnnotation]()
+    
+    @State var checkpoints: [Checkpoint] = [
+        Checkpoint(title: "Musée du Louvre", coordinate: .init(latitude: 48.8611473, longitude: 2.3380277)),
+        Checkpoint(title: "Tour Eiffel", coordinate: .init(latitude: 48.8582602, longitude: 2.2944991)),
+        Checkpoint(title: "Moulin Rouge", coordinate: .init(latitude: 48.8840787, longitude: 2.3324082)),
+        Checkpoint(title: "Place de la Bastille", coordinate: .init(latitude: 48.852987, longitude: 2.3688579)),
+        Checkpoint(title: "Arc de Triomphe", coordinate: .init(latitude: 48.8737791, longitude: 2.2950372))
+    ]
+    
+    
+    var body: some View {
+        ZStack {
+            //            MapView(centerCoordinate: $centerCoordinate, annotations: locations)
+            MapView(checkpoints: $checkpoints)
+                .edgesIgnoringSafeArea(.all)
+            
+            Spacer()
+            
+            Button(action: {
+                
+            }, label: {
+                ZStack {
+                    Image(systemName: "location.circle.fill")
+                        .resizable()
+                        .foregroundColor(.orange)
+                        .zIndex(1)
+                    Circle()
+                        .frame(width: 36, height: 36)
+                        .foregroundColor(.white)
+                        .zIndex(0)
+                }
+                
+                
+                
+            })
+                .frame(width: 40, height: 40)
+                .offset(x: 150, y: 380)
+            
+            //            Circle()
+            //                .fill(Color.blue)
+            //                .opacity(0.3)
+            //                .frame(width: 32, height: 32)
+            //            VStack {
+            //                Spacer()
+            //                HStack {
+            //                    Spacer()
+            //                    Button(action: {
+            //                        let newLocation = MKPointAnnotation()
+            //                        newLocation.coordinate = self.centerCoordinate
+            //                        self.locations.append(newLocation)
+            //                    }, label: {
+            //                        Image(systemName: "plus")
+            //
+            //                    })
+            //                    .padding()
+            //                        .background(Color.black.opacity(0.7))
+            //                        .foregroundColor(.white)
+            //                        .font(.title)
+            //                    .clipShape(Circle())
+            //                        .padding(.trailing)
+            //                }
+            //            }
+            
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
